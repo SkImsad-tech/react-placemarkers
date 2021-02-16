3a47f6a1bc7c850b2f426895024f3a3797cf4d59
SkIm <SkImsad@gmail.com>
2021-02-16 19:26:43 +0500

    add marker event on click

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,5 +1,7 @@
 import { Loader } from "@googlemaps/js-api-loader"
-const center: google.maps.LatLngLiteral = { lat: 56.839, lng: 60.608 };
+let map:google.maps.Map;
+const Yekaterinburg: google.maps.LatLngLiteral = { lat: 56.839, lng: 60.608 };
+const Moscow: google.maps.LatLngLiteral = { lat: 55.750, lng: 37.617 };
 
 export default function initMap(): void {
     const loader = new Loader({
@@ -9,11 +11,23 @@ export default function initMap(): void {
     });
 
     loader.load().then(() => {
-        const map:google.maps.Map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-            center,
-            zoom: 14,
+        map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+            center: Yekaterinburg,
+            zoom: 5,
         });
 
-        return map
+        map.addListener('click', (event: any) => {
+            putMarker(event.latLng);
+        })
+
+        putMarker(Yekaterinburg)
+    });
+
+}
+
+function putMarker (position: google.maps.LatLngLiteral) {
+    new google.maps.Marker({
+        position,
+        map: map,
     });
 }
